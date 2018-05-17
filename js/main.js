bfb4fb0bf5954c8a0a84f185f934c316be8ef70c
Mike Moore <mikemoore1618@gmail.com>
2018-05-17 12:28:19 -0700

    add and remove listings

--- js/main.js
+++ js/main.js
@@ -6,8 +6,22 @@ var $form = $('form')
 
 $form.on('submit', function(evt) {
     evt.preventDefault()
-    var adressValue = $address.val()
+    var addressValue = $address.val()
     var bedroomsValue = $bedrooms.val()
     var bathroomsValue = $bathrooms.val()
-    console.log(adressValue)
+    console.log(addressValue)
+    $list.append(`<li style="display:none">Address: ${addressValue} Bedrooms: ${bedroomsValue} Bathrooms: ${bathroomsValue}</li>`)
+    $('li').last().fadeIn()
+})
+
+
+//jQuery event delegation! 
+//if user clicks anywhere in the list, 
+//'this' refers to a specific thing within
+//that was clicked
+
+$list.on('click','li', function() {
+    $(this).fadeOut(function() {
+        $('this').remove()
+    })
 })
